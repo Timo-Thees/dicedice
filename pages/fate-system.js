089d53871c8402d1f6c1ca1fa5f535c30024e687
Not Yourbusiness <timo.thees@gmx.net>
2023-04-04 12:29:23 +0200

    initial build

---
+++ pages/fate-system.js
@@ -0,0 +1,25 @@
+export default function Fate(){
+    return(
+        <form>
+            <div>
+            <h4>boni</h4>
+            <p>Gelb</p>
+            <input type="number"></input>
+            <p>Grün</p>
+            <input type="number"></input>
+            <p>Blau</p>
+            <input type="number"></input>
+            </div>
+            <div>
+            <h4>mali</h4>
+            <p>Rot</p>
+            <input type="number"></input>
+            <p>Lila</p>
+            <input type="number"></input>
+            <p>Schwarz</p>
+            <input type="number"></input>
+            </div>
+            <button type="submit">roll 'em bones!</button>
+        </form>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Heading from "../components/Heading";
+import styled from "styled-components";
+import Fate from "./fate-system";
 
 export default function Home() {
   return (
     <main>
-      <Heading>🔱Atlantis Capstone Template🔱</Heading>
+      <Heading>DiceDice</Heading>
+      <Subheading>My silly little TTRPG dice helper</Subheading>
+      <Fate/>
     </main>
   );
 }
+
+const Subheading = styled.h3`
+text-align: center;
+`;
