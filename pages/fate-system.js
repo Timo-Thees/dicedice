c53f3edcb102ce3d7ad9acb465fa9820b29a1a0f
Not Yourbusiness <timo.thees@gmx.net>
2023-04-11 12:27:33 +0200

    fixed preventdefault

--- pages/fate-system.js
+++ pages/fate-system.js
@@ -1,28 +1,52 @@
 import styled from "styled-components"
+import { useState, useEffect } from "react";
 
 export default function Fate(){
+    const [dice, setDice] = useState({yellow: 0, green: 0, blue: 0, red: 0, purple: 0, black: 0})
+    const rollDice = (event) =>{
+        event.preventDefault();
+    const yellow = (event.target.yellow.value === ""? 0 : event.target.yellow.value * 1)
+    const green = (event.target.green.value === ""? 0 : event.target.green.value * 1)
+    const blue = (event.target.blue.value === ""? 0 : event.target.blue.value * 1)
+    const red = (event.target.red.value === ""? 0 : event.target.red.value * 1)
+    const purple = (event.target.purple.value === ""? 0 : event.target.purple.value * 1)
+    const black = (event.target.black.value === ""? 0 : event.target.black.value * 1)
+        setDice({yellow: yellow, green: green, blue: blue, red: red, purple: purple, black: black})
+        console.log(dice);
+    }
+   /* useEffect(() => {
+        console.log(dice)
+    }, [
+        setDice()
+    ])*/
     return(
         <div>
-        <StatsEntry>
+        <StatsEntry onSubmit={rollDice}>
             <OuterFlexbox>
-            <div>
+            <InnerFlexbox>
             <h4>boni</h4>
+            <img src="/yellow_dice.png" alt="yellow d12"/>
             <p>Gelb</p>
-            <input type="number" min="0" placeholder="0"></input>
+            <input type="number" min="0" placeholder="0" name="yellow"></input>
+            <img src="/green_dice.png" alt="green d8"/>
             <p>Grün</p>
-            <input type="number" min="0" placeholder="0"></input>
+            <input type="number" min="0" placeholder="0" name="green"></input>
+            <img src="/blue_dice.png" alt="blue d6"/>
             <p>Blau</p>
-            <input type="number" min="0" placeholder="0"></input>
-            </div>
-            <div>
+            <input type="number" min="0" placeholder="0" name="blue"></input>
+            </InnerFlexbox>
+            <InnerFlexbox>
             <h4>mali</h4>
+            <img src="/red_dice.png" alt="red d12"/>
             <p>Rot</p>
-            <input type="number" min="0" placeholder="0"></input>
+            <input type="number" min="0" placeholder="0" name="red"></input>
+            <img src="/purple_dice.png" alt="purple d8"/>
             <p>Lila</p>
-            <input type="number" min="0" placeholder="0"></input>
+            <input type="number" min="0" placeholder="0" name="purple"></input>
+            <img src="/black_dice.png" alt="black d6"/>
             <p>Schwarz</p>
-            <input type="number" min="0" placeholder="0"></input>
-            </div>
+            <input type="number" min="0" placeholder="0" name="black"></input>
+            </InnerFlexbox>
             </OuterFlexbox>
             <button type="submit">roll 'em bones!</button>
         </StatsEntry>
@@ -48,7 +72,13 @@ align-items: center;
 const OuterFlexbox = styled.div`
 display: flex;
 flex-direction: row;
+`;
+
+const InnerFlexbox = styled.div`
+display: flex;
+flex-direction: column;
 text-align: center;
+align-items: center;
 `;
 
 const ResultsContainer = styled.div`
