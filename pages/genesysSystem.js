4904e33220827720bcf474ccc0b219a4304b0ba6
Not Yourbusiness <timo.thees@gmx.net>
2023-04-20 15:40:15 +0200

    added negating arrays

--- pages/genesysSystem.js
+++ pages/genesysSystem.js
@@ -9,8 +9,7 @@ export default function Genesys(){
     const [disadvantage, setDisadvantage] = useState([])
     const [failure, setFailure] = useState([])
     const [disaster, setDisaster] = useState([])
-    const [successesAndTriumphs, setSuccessesAndTriumphs] = useState()
-    const [failuresAndDisasters, setFailuresAndDisasters] = useState()
+    const [diceResults, setDiceResults] = useState([])
 
     const rollDice = (event) =>{
         event.preventDefault();
@@ -27,8 +26,7 @@ export default function Genesys(){
         setDisadvantage([]);
         setFailure([]);
         setDisaster([]);
-        setSuccessesAndTriumphs();
-        setFailuresAndDisasters();
+        setDiceResults([]);
     }
 
    useEffect(() => {
@@ -39,20 +37,25 @@ export default function Genesys(){
         if(dice.yellow > 0){
             const yellowDiceRemaining = dice.yellow - 1;
             const yellowResult = getRandomInteger(1, 13);
-            if (yellowResult > 1 && yellowResult < 9){
+            if (yellowResult >= 2 && yellowResult <= 3){
                 setSuccesses([...successes, {key: successes.length +1, negated: false}])
             }
-            if (yellowResult > 3 && yellowResult < 6){
+            if (yellowResult >= 4 && yellowResult <= 5){
+                setSuccesses([...successes, {key: successes.length +1, negated: false}, {key: successes.length +2, negated: false}])
+            }
+            if (yellowResult === 6){
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
+            }
+            if (yellowResult >= 7 && yellowResult <= 9){
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false}]);
                 setSuccesses([...successes, {key: successes.length +1, negated: false}])
             }
-            if (yellowResult > 5 && yellowResult < 12){
-                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
-            }
-            if (yellowResult > 9 && yellowResult < 12){
-                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
+            if (yellowResult >= 10 && yellowResult <= 11){
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false}, {key: advantage.length +2, negated: false}])
             }
             if (yellowResult === 12){
-                setTriumphs([...triumphs, {key: triumphs.length +1, negated: false}])
+                setTriumphs([...triumphs, {key: triumphs.length +1}])
+                setSuccesses([...successes, {key: successes.length +1, negated: false}])
             }
             setDice({...dice, yellow: yellowDiceRemaining});
             return;
@@ -60,89 +63,112 @@ export default function Genesys(){
         if(dice.green > 0){
             const greenDiceRemaining = dice.green -1;
             const greenResult = getRandomInteger(1, 9);
-            if (greenResult > 1 && greenResult < 6){
+            if (greenResult >= 2 && greenResult <= 3){
                 setSuccesses([...successes, {key: successes.length +1, negated: false}])
             }
             if (greenResult === 4){
+                setSuccesses([...successes, {key: successes.length +1, negated: false}, {key: successes.length +2, negated: false}])
+            }
+            if (greenResult >= 5 && greenResult <= 6){
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
+            }
+            if (greenResult === 7){
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
                 setSuccesses([...successes, {key: successes.length +1, negated: false}])
             }
-            if (greenResult > 4){
-                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
-            }
             if (greenResult === 8){
-                setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false}, {key: advantage.length +2, negated: false}])
             }
             setDice({...dice, green: greenDiceRemaining - 1});
+            setDiceResults([...diceResults, greenResult])
             return;
         }
         if(dice.blue > 0){
             const blueDiceRemaining = dice.blue -1
             const blueResult = getRandomInteger(1, 7);
-            if (blueResult > 2 && blueResult < 5){
+            if (blueResult === 3){
                 setSuccesses([...successes, {key: successes.length +1, negated: false}])
             }
-            if (blueResult > 3){
+            if (blueResult === 4){
+                setSuccesses([...successes, {key: successes.length +1, negated: false}])
                 setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
             }
             if (blueResult === 5){
+                setAdvantage([...advantage, {key: advantage.length +1, negated: false},{key: advantage.length +2, negated: false}])
+            }
+            if (blueResult === 6){
                 setAdvantage([...advantage, {key: advantage.length +1, negated: false}])
             }
             setDice({...dice, blue: blueDiceRemaining - 1});
+            setDiceResults([...diceResults, blueResult])
             return;
         }
         if(dice.red > 0){
             const redDiceRemaining = dice.red -1
             const redResult = getRandomInteger(1, 13)
-            console.log("Würfelergebnis:")
-            console.log(redResult)
-            if (redResult > 1 && redResult < 8){
+            if (redResult >= 2 && redResult <= 3){
                 setFailure([...failure, {key: failure.length +1, negated: false}])
             }
-            if (redResult > 3 && redResult < 6){
+            if (redResult >= 4 && redResult <= 5){
+                setFailure([...failure, {key: failure.length +1, negated: false},{key: failure.length +2, negated: false}])
+            }
+            if (redResult >= 6 && redResult <= 7){
+                setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
+            }
+            if (redResult >= 8 && redResult <= 9){
+                setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
                 setFailure([...failure, {key: failure.length +1, negated: false}])
             }
-            if (redResult > 5 && redResult < 12){
-                setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
-            }
-            if (redResult > 9 && redResult < 12){
-                setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
+            if (redResult >= 10 && redResult <= 11){
+                setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}, {key: disadvantage.length +2, negated: false}])
             }
             if (redResult === 12){
-                setDisaster([...disaster, {key: disaster.length +1, negated: false}])
+                setDisaster([...disaster, {key: disaster.length +1}])
+                setFailure([...failure, {key: failure.length +1, negated: false}])
             }
             setDice({...dice, red: redDiceRemaining});
+            setDiceResults([...diceResults, redResult])
             return;
         }
         if(dice.purple > 0){
             const purpleDiceRemaining = dice.purple -1
             const purpleResult = getRandomInteger(1, 9)
-            if (purpleResult > 1 && purpleResult < 5){
+            if (purpleResult === 2){
                 setFailure([...failure, {key: failure.length +1, negated: false}])
             }
             if (purpleResult === 3){
-                setFailure([...failure, {key: failure.length +1, negated: false}])
+                setFailure([...failure, {key: failure.length +1, negated: false}, {key: failure.length +2, negated: false}])
             }
-            if (purpleResult > 3){
+            if (purpleResult >= 4 && purpleResult <= 6){
                 setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
+            }
+            if (purpleResult === 7){
+                setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}, {key: disadvantage.length +2, negated: false}])
             }
             if (purpleResult === 8){
                 setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
+                setFailure([...failure, {key: failure.length +1, negated: false}])
             }
             setDice({...dice, purple: purpleDiceRemaining});
+            setDiceResults([...diceResults, purpleResult]);
             return;
         }
         if(dice.black > 0){
             const blackDiceRemaining = dice.black -1
             const blackResult = getRandomInteger(1, 7)
-            if (blackResult > 2 && blackResult < 5){
+            if (blackResult >= 3 && blackResult <= 4){
                 setFailure([...failure, {key: failure.length +1, negated: false}])
             }
             if (blackResult > 4){
                 setDisadvantage([...disadvantage, {key: disadvantage.length +1, negated: false}])
             }
             setDice({...dice, black: blackDiceRemaining});
+            setDiceResults([...diceResults, blackResult]);
             return;
         }
+        negateOpposites(advantage, setAdvantage, disadvantage, setDisadvantage);
+        negateOpposites(successes, setSuccesses, failure, setFailure);
+        console.log(successes, failure);
     }
 
     function getRandomInteger(min, max){
@@ -151,31 +177,41 @@ export default function Genesys(){
         return Math.floor(Math.random() * (max - min) + min)
     }
 
-    function negateAdvantageDisadvantage(){
-        const numberOfAdvantages = advantage.length
-        const numberOfDisadvantages = disadvantage.length
-        if (numberOfAdvantages >= numberOfDisadvantages){
-            setAdvantage(advantage.forEach(negateAdvantages));
-            setDisadvantage(disadvantage.forEach(negateAdvantages));
-            function negateAdvantages(value){
-                if(value.key <= numberOfDisadvantages){
-                    return({...value, negated: true})
-                }
-            }
-        } else {
-            setAdvantage(advantage.forEach(negateDisadvantages));
-            setDisadvantage(disadvantage.forEach(negateDisadvantages));
-            function negateDisadvantages(value){
-                if(value.key <= numberOfAdvantages){
-                    return({...value, negated: true})
-                }
+
+    function negateOpposites(firstArray, setFirstArray, secondArray, setSecondArray){
+        const compareArrayLength = [firstArray.length, secondArray.length];
+        compareArrayLength.sort(function(a, b){return b - a});
+        const smallerArray = compareArrayLength.pop();
+        let firstArrayNegated = []
+        let firstArrayNotNegated = []
+        let secondArrayNegated = []
+        let secondArrayNotNegated = []
+        function negateFirstArray(arrayEntry){
+            if(arrayEntry.key <= smallerArray){
+                firstArrayNegated.push({...arrayEntry, negated: true})
+            } else {
+                firstArrayNotNegated.push(arrayEntry)
             }
         }
+        function negateSecondArray(arrayEntry){
+            if(arrayEntry.key <= smallerArray){
+                secondArrayNegated.push({...arrayEntry, negated: true})
+            } else {
+                secondArrayNotNegated.push(arrayEntry)
+            }
+        }
+        firstArray.map(negateFirstArray);
+        secondArray.map(negateSecondArray);
+        setFirstArray([...firstArrayNegated, ...firstArrayNotNegated])
+        setSecondArray([...secondArrayNegated, ...secondArrayNotNegated])
     }
 
-
+    const showArrays = () => {
+        console.log(successes, failure)
+    }
     return(
         <div>
+            <button onClick={showArrays}>show successes/failures arrays</button>
         <StatsEntry onSubmit={rollDice}>
             <OuterFlexbox>
             <InnerFlexbox>
@@ -209,8 +245,7 @@ export default function Genesys(){
         <ResultsRow>
             <p>Triumphe</p>
             {triumphs.map(result => {
-                if(result.negated === false){
-                return(
+              {return(
                     <img key={result.key} src="/triumph.png" alt="Triumph!" />
                 )}
             })}
@@ -218,33 +253,61 @@ export default function Genesys(){
         <ResultsRow>
             <p>Erfolge</p>
             {successes.map(result => {
+                if(result.negated === true){
+                return(
+                    <img key={result.key} src="/erfolg-neutralised.png" alt="Success was neutralised" />
+                    
+                )
+                } else {
                 return(
                     <img key={result.key} src="/erfolg.png" alt="Success" />
-                )
+                )    
+                }
             })}
         </ResultsRow>
         <ResultsRow>
             <p>Misserfolge</p>
             {failure.map(result => {
+                if(result.negated === true){
+                return(
+                    <img key={result.key} src="/misserfolg-neutralised.png" alt="Failure was neutralised" />
+                    
+                )
+                } else {
                 return(
                     <img key={result.key} src="/misserfolg.png" alt="Failure" />
-                )
+                )    
+                }
             })}
         </ResultsRow>
         <ResultsRow>
             <p>Chanchen</p>
             {advantage.map(result => {
+                if(result.negated === true){
                 return(
-                    <img key={result.key} src="/chance.png" alt="Andvantage" />
+                    <img key={result.key} src="/chance-neutralised.png" alt="Advantage was neutralised" />
+                    
                 )
+                } else {
+                return(
+                    <img key={result.key} src="/chance.png" alt="Advantage" />
+                )    
+                }
             })}
         </ResultsRow>
         <ResultsRow>
             <p>Bedrohungen</p>
             {disadvantage.map(result => {
+                if(result.negated === true){
                 return(
-                    <img key={result.key} src="/bedrohung.png" alt="Disadvantage" />
+                    <img key={result.key} src="/bedrohung-neutralised.png" alt="Disadvantage was neutralised" />
+                    
                 )
+                } else {
+                return(
+                    <img key={result.key} src="/bedrohung.png" alt="disadvantae" />
+                )    
+                }
             })}
         </ResultsRow>
         <ResultsRow>
