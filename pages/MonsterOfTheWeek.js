20621edea6f6f7f5fa4b7d81a77dd9d8a813a320
Not Yourbusiness <timo.thees@gmx.net>
2023-05-05 17:48:16 +0200

    add moves aray for MOTW

--- pages/MonsterOfTheWeek.js
+++ pages/MonsterOfTheWeek.js
@@ -2,9 +2,11 @@ import Heading from "../components/Heading";
 import {InnerFlexbox, OuterFlexbox, StatsEntry} from "../components/design"
 import { useState, useEffect } from "react";
 import { diceButtonTextArray } from "../components/diceButtonText";
+import { monsterOfTheWeekMoveList } from "../components/monsterOfTheWeekMovelist";
 
 export default function MonsterOfTheWeek({setPage}){
     const [diceButtonText, setDiceButtonText] = useState("Klick to roll dice")
+    const [resultOverlay, setResultOverlay] = useState(false)
 
     function getRandomInteger(min, max){
         min = Math.ceil(min)
@@ -16,63 +18,47 @@ export default function MonsterOfTheWeek({setPage}){
         setDiceButtonText(diceButtonTextArray[getRandomInteger(0, diceButtonTextArray.length)])
     }
 
+    useEffect(() => {
+        changeDiceText()
+    }, [])
+
     const handleRollDice = () =>{
         event.preventDefault();
+        console.log(event.target.form.advanced)
+        const modifier = event.target.form.modifier.value
+        const diceResult = getRandomInteger(1,7) + getRandomInteger(1,7) + modifier
         changeDiceText();
+        setResultOverlay(true);
     }
 
     return(
         <div>
             <Heading>Monster of the Week</Heading>
         	<StatsEntry>
-                <h3>Set your modefier</h3>
-                <input type="number" min="-2" max="4" placeholder="0"></input>
+                <h3>Set your modifier</h3>
+                <input type="number" min="-2" max="4" placeholder="0" id="modifier"></input>
                 <input type="checkbox" id="advanced"></input>
                 <label for="advanced">This move is advanced</label>
                 <button onClick={handleRollDice}>{diceButtonText}</button>
                 <h4>Select your move</h4>
                 <OuterFlexbox>
                     <InnerFlexbox>
-                        <input type="radio" id="Playbook Move" name="move"></input>
-                        <label for="Playbook Move">A move from your playbook</label><br/>
-                        <input type="radio" id="Act under Pressure" name="move"></input>
-                        <label for="Act under Pressure">Act under Pressure</label><br/>
-                        <input type="radio" id="Kick some Ass" name="move"></input>
-                        <label for="Kick some Ass">Kick some Ass</label><br/>
-                        <input type="radio" id="Help out" name="move"></input>
-                        <label for="Help out">Help out</label><br/>
-                        <input type="radio" id="Protect someone" name="move"></input>
-                        <label for="Protect someone">Protect someone</label><br/>
-                        <input type="radio" id="Investigate a Mystery" name="move"></input>
-                        <label for="Investigate a Mystery">Investigate a Mystery</label><br/>
-                        <input type="radio" id="Read a Bad Situation" name="move"></input>
-                        <label for="Read a Bad Situation">Read a Bad Situation</label><br/>
-                        <input type="radio" id="Manipulate Someone" name="move"></input>
-                        <label for="Manipulate Someone">Manipulate Someone</label><br/>
+                        <OuterFlexbox>
+                                <input type="radio" id="Just result" name="move"/>
+                                <label htmlFor="Just result">Just roll dice</label>
+                        </OuterFlexbox>
+                        {monsterOfTheWeekMoveList.map(move =>{
+                            return(
+                                <OuterFlexbox key={move.key}>
+                                    <input type="radio" id={move.name} name="move"/>
+                                    <label htmlFor={move.name}>{move.name}</label>
+                                    <p>{move.tooltip}</p><br/>
+                                    <p>"Atribut: "{move.rollWith}</p>
+                                </OuterFlexbox>
+                            )
+                        })}
                     </InnerFlexbox>
-                    <InnerFlexbox>
-                        <input type="radio" id="Use Magic" name="move"></input>
-                        <label for="Use Magic">Use Magic</label><br/>
-                        <input type="radio" id="Empath" name="move"></input>
-                        <label for="Empath">Empath</label><br/>
-                        <input type="radio" id="Illuminated" name="move"></input>
-                        <label for="Illuminated">Illuminated</label><br/>
-                        <input type="radio" id="No Limits" name="move"></input>
-                        <label for="No Limits">No Limits</label><br/>
-                        <input type="radio" id="Past Lives" name="move"></input>
-                        <label for="Past Lives">Past Lives</label><br/>
-                        <input type="radio" id="Sensitive" name="move"></input>
-                        <label for="Sensitive">Sensitive</label><br/>
-                        <input type="radio" id="Trust your Gut" name="move"></input>
-                        <label for="Trust your Gut">Trust your Gut</label><br/>
-                        <input type="radio" id="Telekinesis" name="move"></input>
-                        <label for="Telekinesis">Telekinesis</label><br/>
-                        <input type="radio" id="Weird Science" name="move"></input>
-                        <label for="Weird Science">Weird Science</label><br/>
-                    </InnerFlexbox>
-                </OuterFlexbox>
-                        
-                        
+                </OuterFlexbox>                        
             </StatsEntry>
             <button onClick={()=> setPage("home")}>Home</button>
         </div>
