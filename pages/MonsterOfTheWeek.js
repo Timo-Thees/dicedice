07c8a3c0f76a6b96352869ab49b9b80a1f485821
Not Yourbusiness <timo.thees@gmx.net>
2023-05-06 23:42:14 +0200

    added result overlay for motw

--- pages/MonsterOfTheWeek.js
+++ pages/MonsterOfTheWeek.js
@@ -3,10 +3,14 @@ import {InnerFlexbox, OuterFlexbox, StatsEntry} from "../components/design"
 import { useState, useEffect } from "react";
 import { diceButtonTextArray } from "../components/diceButtonText";
 import { monsterOfTheWeekMoveList } from "../components/monsterOfTheWeekMovelist";
+import {MonsterOfTheWeekResultOverlay} from "./motwOverlay"
 
 export default function MonsterOfTheWeek({setPage}){
     const [diceButtonText, setDiceButtonText] = useState("Klick to roll dice")
     const [resultOverlay, setResultOverlay] = useState(false)
+    const [selectMove, setSelectMove] = useState()
+    const [diceResult, setDiceResult] = useState()
+    const [isAdvanced, setIsAdvanced] = useState(false)
 
     function getRandomInteger(min, max){
         min = Math.ceil(min)
@@ -25,32 +29,33 @@ export default function MonsterOfTheWeek({setPage}){
     const handleRollDice = () =>{
         event.preventDefault();
         console.log(event.target.form.advanced)
-        const modifier = event.target.form.modifier.value
-        const diceResult = getRandomInteger(1,7) + getRandomInteger(1,7) + modifier
+        const modifier = (event.target.form.modifier.value === "" ? 0 : event.target.form.modifier.value*1)
+        setDiceResult({firstDice: getRandomInteger(1,7), secondDice: getRandomInteger(1,7), modifier: modifier, selectedMove: selectMove, isAdvanced: isAdvanced})
         changeDiceText();
         setResultOverlay(true);
     }
 
     return(
         <div>
+            {resultOverlay === true ? (
+            <MonsterOfTheWeekResultOverlay diceResult={diceResult} setResultOverlay={setResultOverlay}/>
+) : (<></>)
+            }
             <Heading>Monster of the Week</Heading>
         	<StatsEntry>
                 <h3>Set your modifier</h3>
                 <input type="number" min="-2" max="4" placeholder="0" id="modifier"></input>
-                <input type="checkbox" id="advanced"></input>
+                <input type="checkbox" id="advanced" onChange={()=>setIsAdvanced(!isAdvanced)}></input>
                 <label for="advanced">This move is advanced</label>
+                <p>is this move advanced? {isAdvanced.toString()}</p>
                 <button onClick={handleRollDice}>{diceButtonText}</button>
                 <h4>Select your move</h4>
                 <OuterFlexbox>
                     <InnerFlexbox>
-                        <OuterFlexbox>
-                                <input type="radio" id="Just result" name="move"/>
-                                <label htmlFor="Just result">Just roll dice</label>
-                        </OuterFlexbox>
                         {monsterOfTheWeekMoveList.map(move =>{
                             return(
                                 <OuterFlexbox key={move.key}>
-                                    <input type="radio" id={move.name} name="move"/>
+                                    <input type="radio" id={move.name} name="move" value={move.key} onChange={()=>setSelectMove(move.key)}/>
                                     <label htmlFor={move.name}>{move.name}</label>
                                     <p>{move.tooltip}</p><br/>
                                     <p>"Atribut: "{move.rollWith}</p>
---
+++ pages/motwOverlay.js
@@ -0,0 +1,79 @@
+import Heading from "../components/Heading";
+import styled from "styled-components";
+import { monsterOfTheWeekMoveList } from "../components/monsterOfTheWeekMovelist";
+
+export function MonsterOfTheWeekResultOverlay({diceResult, setResultOverlay}){
+    const selectedMove = monsterOfTheWeekMoveList.map(move =>{if(move.key === diceResult.selectedMove){return({...move})}})
+    const totalResult = diceResult.firstDice + diceResult.secondDice + diceResult.modifier
+    const tooltipText = ()=>{
+        if(totalResult <= 6 && selectedMove.onFail === "Standart"){
+            return(
+                "Failure. Mark expirience and accept the consequences"
+            )
+        } else if(totalResult <= 6 && selectedMove.onFail !== "Standart") {
+            return(
+                selectedMove.onFail
+            )
+
+        } else if(totalResult >= 7 && totalResult <= 9){
+            return(
+                selectedMove.onMixed
+            )
+        } else if(totalResult >=10 && totalResult <= 11){
+            return(
+                selectedMove.onSuccsess
+            )
+        } else if(totalResult >=12 && diceResult.isAdvanced === true && totalResult.onAdvanced !== false){
+            return(
+                selectedMove.onAdvanced
+            )
+        } else if(totalResult >=12){
+            return(
+                selectedMove.onSuccsess
+            )
+        }
+           
+    }
+    return(
+        <Overlay>
+            <Dialog>
+                <Heading>{totalResult}</Heading>
+                <h2>firstDice: {diceResult.firstDice}, secondDice: {diceResult.secondDice}, modifier: {diceResult.modifier}</h2>
+                <h3>is Advanced: {diceResult.isAdvanced.toString()}</h3>
+                <a>here is the text: {tooltipText}</a>
+                <button onClick={() => setResultOverlay(false)}>ok enough of that</button>
+                <button onClick={()=> console.log(selectedMove)}>What is in selectedMove?</button>
+            </Dialog>
+        </Overlay>
+    )
+}
+
+const Overlay = styled.div`
+  --tw-bg-opacity: 1;
+  background-color: rgba(0, 0, 0, var(--tw-bg-opacity));
+  --tw-bg-opacity: 0.5;
+  height: 100%;
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  z-index: 10;
+`;
+
+const Dialog = styled.div`
+  --tw-bg-opacity: 1;
+  background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
+  border-radius: 0.75rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2.5rem;
+  max-width: 100%;
+  width: 70vw;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`;
