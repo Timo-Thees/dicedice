e9ce12f32b1b162a6409238cc558e78790ff9ee0
Not Yourbusiness <timo.thees@gmx.net>
2023-05-11 15:25:54 +0200

    bug hunt

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,2 @@
 import Home from "../pages";
 import { render, screen } from "@testing-library/react";
-
-test("test", () => {
-  render(<Home />);
-  const element = screen.getByRole("heading", {
-    name: /🔱/,
-  });
-  expect(element).toBeInTheDocument();
-});
